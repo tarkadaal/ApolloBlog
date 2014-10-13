1b255435c0089e6a8d37142c0b7c6460abc01477
Dan Jackson <danjacksonis@googlemail.com>
2014-10-13 21:39:45 +0100

    Added string rep for model.

--- ApolloBlog/blog/models.py
+++ ApolloBlog/blog/models.py
@@ -4,3 +4,6 @@ class Article(models.Model):
 	title = models.CharField('Title', max_length=255)
 	created_date = models.DateTimeField('Publication Date')
 	body = models.TextField('Body')
+
+	def __unicode__(self):
+		return self.title
