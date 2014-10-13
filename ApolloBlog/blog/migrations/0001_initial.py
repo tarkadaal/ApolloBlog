ee367c37f1e4618b265acb203a0fa42071a09bd6
Dan Jackson <danjacksonis@googlemail.com>
2014-10-13 21:15:19 +0100

    Added first model and migration.

---
+++ ApolloBlog/blog/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255, verbose_name=b'Title')),
+                ('created_date', models.DateTimeField(verbose_name=b'Publication Date')),
+                ('body', models.TextField(verbose_name=b'Body')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- ApolloBlog/blog/models.py
+++ ApolloBlog/blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+	title = models.CharField('Title', max_length=255)
+	created_date = models.DateTimeField('Publication Date')
+	body = models.TextField('Body')
