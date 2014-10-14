300fad3f6e49c8d803740838ab97a66aa4f3eea1
Dan Jackson <danjacksonis@googlemail.com>
2014-10-14 10:29:41 +0100

    Added simple post creation interface

--- ApolloBlog/blog/urls.py
+++ ApolloBlog/blog/urls.py
@@ -3,5 +3,7 @@ from blog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^compose[/]*$', views.compose, name='compose'),
+    url(r'^create[/]*$', views.create, name='create'),
     url(r'^(?P<article_id>\d+)/$', views.detail, name='detail'),
 )
