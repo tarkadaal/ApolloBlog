633de184912a6b97e38a00a519ba989856332e7a
Dan Jackson <danjacksonis@googlemail.com>
2014-10-14 09:02:01 +0100

    Added details url to show content of blog posts.

--- ApolloBlog/blog/views.py
+++ ApolloBlog/blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blog.models import Article
+
 def index(request):
-	return HttpResponse('Hello, world, is it me you\'re looking for?')
+	latest_articles = Article.objects.order_by('-created_date')[:5]
+	context =  {'latest_articles': latest_articles}
+	return render(request, 'blog/index.html', context)
+
+def detail(request, article_id):
+	article = Article.objects.get(pk=article_id)
+	context = {'article': article}
+	return render(request, 'blog/details.html', context)
