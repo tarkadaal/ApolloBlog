ff466e120745c41efafac966f9fe00e584b3669e
Dan Jackson <danjacksonis@googlemail.com>
2014-10-15 19:41:47 +0100

    Added editing and deleting to blog.

--- ApolloBlog-nonrel/blog/urls.py
+++ ApolloBlog-nonrel/blog/urls.py
@@ -6,4 +6,8 @@ urlpatterns = patterns('',
     url(r'^compose[/]*$', views.compose, name='compose'),
     url(r'^create[/]*$', views.create, name='create'),
     url(r'^(?P<article_id>\d+)/$', views.detail, name='detail'),
+    url(r'^edit/(?P<article_id>\d+)[/]*$', views.edit, name='edit'),
+    url(r'^delete/(?P<article_id>\d+)[/]*$', views.delete, name='delete'),
+    url(r'^commit_edit[/]*$', views.commit_edit, name='commit_edit'),
+    url(r'^commit_delete[/]*$', views.commit_delete, name='commit_delete'),
 )
--- ApolloBlog-nonrel/blog/views.py
+++ ApolloBlog-nonrel/blog/views.py
@@ -31,3 +31,28 @@ def create(request):
 		created_date=datetime.datetime.now())
 	new_article.save()
 	return HttpResponseRedirect(reverse('blog:detail', args=(new_article.id, )))
+
+@login_required
+def edit(request, article_id):
+	article = get_object_or_404(Article, pk=article_id)
+	context = {'article': article }
+	return render(request, 'blog/edit.html', context)
+
+def commit_edit(request):
+	article = get_object_or_404(Article, pk= request.POST['id'])
+	article.title = request.POST['title']
+	article.body = request.POST['body']
+	article.save()
+	return HttpResponseRedirect(reverse('blog:detail', args=(article.id, )))
+
+@login_required
+def delete(request, article_id):
+	article = get_object_or_404(Article, pk=article_id)
+	context = {'article': article }
+	return render(request, 'blog/delete.html', context)
+	
+@login_required
+def commit_delete(request):
+	article = get_object_or_404(Article, pk=request.POST['id'])
+	article.delete()
+	return HttpResponseRedirect(reverse('blog:index'))
